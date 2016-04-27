9a6394960507e60bfe82c192b25614f1ffb5a3eb
Estefania Flores <eflores0227@gmail.com>
2016-04-27 14:01:45 -0700

    checkpoint 25, assignment

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,17 @@
      ]
  };
 
+var albumGeorgeEzra = {
+    title: 'Wanted on Voyage',
+    artist: 'George Ezra',
+    label: 'Columbia',
+    year: '2014',
+    albumArtUrl: 'assests/images/Wanted_on_Voyage.jpg',
+    songs: [
+        { title: 'Barcelona', duration: '3:03' }
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -40,13 +51,14 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+
  var setCurrentAlbum = function(album) {
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -60,4 +72,14 @@ var createSongRow = function(songNumber, songName, songLength) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var albums = [albumPicasso, albumMarconi, albumGeorgeEzra];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+         setCurrentAlbum(album[index]);
+         index++;
+         if (index == index.length) {
+             index = 0;
+         }
+     });
  };
