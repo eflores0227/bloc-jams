fb5bc7e43cc6b6cb3ffc4af964b8201458de0368
Estefania Flores <eflores0227@gmail.com>
2016-04-27 13:28:28 -0700

    checkpoint 25 done

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
- 
-    var animatePoints = function(points) {
- 
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+var revealPoint = function(point) {
+    points.style.opacity = 1;
+    points.style.transform = "scaleX(1) translateY(0)";
+    points.style.msTransform = "scaleX(1) translateY(0)";
+    points.style.WebkitTransform = "scaleX(1) translateY(0)";
      };
  
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
  
-    var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
- 
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
- 
+var animatePoints = function(points) {
+    forEach(points, revealPoints);
 };
 
 window.onload = function() {
@@ -34,9 +17,9 @@ window.onload = function() {
         animatePoints(pointsArray);
     }
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight < 500;
     window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTOp || document.body.scrollTop >= scrollDistance) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
         }
     });
