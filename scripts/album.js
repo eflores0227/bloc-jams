14756055dd5052ca0a3ea96c0ac11f665694f7fe
Estefania Flores <eflores0227@gmail.com>
2016-04-28 10:01:56 -0700

    checkpoint 26

--- scripts/album.js
+++ scripts/album.js
@@ -34,7 +34,7 @@ var albumJustinTimberlake = {
     year: "2004",
     albumArtUrl: "assets/images/album_covers/justin.jpg",
     songs: [
-        {title: "hey" }
+        {title: "Love, Sex, Magic" }
     ]
 };
 
@@ -49,13 +49,13 @@ var createSongRow = function(songNumber, songName, songLength) {
  
      return template;
  };
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
  var setCurrentAlbum = function(album) {
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -71,10 +71,38 @@ var songListContainer = document.getElementsByClassName('album-view-song-list')[
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+var currentlyPlayingSong = null;
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
      
+     var albums = [albumPicasso, albumMarconi, albumJustinTimberlake];
+     var index = 1;
+     albumImage.addEventListener('click', function(event) {
+         setCurrentAlbum(album);
+         index++;
+         if (index == album.length) {
+             index = 0;
+         }
+     });
+     var clickHandler = function(targetElement) {
+         var songItem = getSongItem(targetElement);
+         
+         if (currentlyPlayingSong === null;) {
+             songItem.innerHTML = pauseButtonTemplate;
+             currentlyPlayingSong = songItem.getAttribute('data-song-number');
+         } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+             songItem.innerHTML = playButtonTemplate;
+             currentlyPlayingSong = null;
+         } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+             var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+             currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+             songItem.innerHTML = pauseButtonTemplate;
+             currentlyPlayingSong = songItem.getAttribute('data-song-number');
+         }
+     };
      songListContainer.addEventListener('mouseover', function(event) {
          if (event.target.parentElement.className === 'album-view-song-item') {
              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
@@ -82,7 +110,15 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
      });
      for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            var songItem = getSongItem(event.target);
+            var songItemNumber = songItem.getAttribute('data-song-number');
+             
+             if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
+         });
+         songRows[i].addEventListener('click', function(event) {
+             clickHandler(event.target);
          });
      }
  };
